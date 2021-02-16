8bd34bb0e7a3abb8637bfa48f29459aa671fded4
Edouard Piron <ed.piron@gmail.com>
2021-02-16 22:11:57 +0100

    init CSV printer

---
+++ lib/gems_bond/printers/csv.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "erb"
+require "fileutils"
+require "gems_bond/printers/printer"
+
+module GemsBond
+  module Printers
+    # Prints CSV file
+    class CSV < Printer
+      # Prints data into a CSV file
+      # @return [void]
+      def call
+        # ...
+      end
+
+      # ...
+    end
+  end
+end
---
+++ spec/lib/gems_bond/printers/csv_spec.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+require "gems_bond/printers/csv"
+
+RSpec.describe GemsBond::Printers::CSV do; end # rubocop:disable RSpec/EmptyExampleGroup, Style/BlockDelimiters
